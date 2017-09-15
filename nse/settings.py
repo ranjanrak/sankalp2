0a8e235b99811cac4ad7f1b97fed3cc3c05ca559
Rakesh <ranjan01995@gmail.com>
2017-09-15 17:28:53 +0530

    changes

--- nse/settings.py
+++ nse/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'nse.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    if 'RDS_DB_NAME' in os.environ:
+if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
