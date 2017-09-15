41c75363d9d4688aded713847ecefd364a5f2cea
Rakesh <ranjan01995@gmail.com>
2017-09-15 12:48:43 +0530

    added

--- nse/settings.py
+++ nse/settings.py
@@ -79,15 +79,14 @@ WSGI_APPLICATION = 'nse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres1',
+        'USER': 'postgres',
+        'PASSWORD': 'rakesh123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
