1fd77e55f649408de85594e2be066668c2c4f06d
Rakesh <ranjan01995@gmail.com>
2017-09-17 17:09:26 +0530

    changed database

--- nse/settings.py
+++ nse/settings.py
@@ -77,7 +77,6 @@ WSGI_APPLICATION = 'nse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
     if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
